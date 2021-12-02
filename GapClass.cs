fbddffc90ce3c17e47c7d1578e65f2df16d683ee
Ulf94 <79094141+Ulf94@users.noreply.github.com>
2021-12-02 13:32:41 +0100

    Add project files.

---
+++ GapClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+namespace BinaryGap
+{
+    internal class GapClass
+    {
+        public int BinaryGap(int number)
+        {
+            int N = 0;
+            List<int> gaps = new List<int>();
+            var tempMax = 0;
+
+            Console.Write($"Provided integer number= {number}\n");
+
+            var binaryNumber = Convert.ToString(number, 2);
+
+            Console.Write($"Number converted into binary = {binaryNumber}\n");
+
+            foreach (char c in binaryNumber)
+            {
+                if (c == '0') tempMax += 1;
+                if (c == '1')
+                {
+                    if (tempMax != 0) gaps.Add(tempMax);
+                    tempMax = 0;
+                }
+            }
+            return gaps.Max();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryGap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = new GapClass();
+
+            while (true)
+            {
+                Console.WriteLine("Provide number:");
+                string number = Console.ReadLine();
+                Console.WriteLine(x.BinaryGap(Convert.ToInt32(number)));           
+                Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
